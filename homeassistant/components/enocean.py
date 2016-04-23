8b42f80854abcc26e3bfbfb6ea76583ce0979305
Ruben Undheim <ruben.undheim@gmail.com>
2016-04-23 12:47:21 +0200

    Receive in thread

--- homeassistant/components/enocean.py
+++ homeassistant/components/enocean.py
@@ -1,6 +1,7 @@
 
 import serial
 import threading
+import time
 
 DOMAIN = "enocean"
 
@@ -17,9 +18,33 @@ def setup(hass,config):
     ENOCEAN_DONGLE = EnOceanDongle(hass,"/dev/ttyUSB0")
     return True
 
+
+class EnOceanThread(threading.Thread):
+    def __init__(self,ser,lock,kwargs=None):
+        threading.Thread.__init__(self,kwargs=kwargs)
+        self.__lock = lock
+        self.__ser = ser
+        self.__running = True
+    def run(self):
+        print("\n\nRunning thread\n\n")
+        while self.__running:
+            print("Running: %d" % (self.__running))
+            with self.__lock:
+                data = self.__ser.read()
+            print("\nData recived: "+str(data)+"\n")
+            time.sleep(1)
+    def stop_this_one(self):
+        self.__running = False
+
 class EnOceanDongle:
     def __init__(self,hass,ser):
         self.__ser = serial.Serial(ser, 57600, timeout=0.1)
+        self.__thread = EnOceanThread(self.__ser,ENOCEAN_LOCK)
+        self.__thread.start()
+
+    def __del__(self):
+        self.__thread.stop_this_one()
+        self.__thread.join()
 
     def send_command(self,command):
         with(ENOCEAN_LOCK):
