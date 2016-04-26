c06849b543956efa0ec0dd6ac8a073ef84647837
Ruben Undheim <ruben.undheim@gmail.com>
2016-04-26 22:03:06 +0200

    Adding support for many switches

--- homeassistant/components/switch/enocean.py
+++ homeassistant/components/switch/enocean.py
@@ -5,6 +5,7 @@ import logging
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 from homeassistant.components import enocean
 from homeassistant.helpers.entity import ToggleEntity
+from homeassistant.const import ATTR_ENTITY_ID
 
 # Home Assistant depends on 3rd party packages for API specific code.
 #REQUIREMENTS = ['awesome_lights==1.2.3']
@@ -21,6 +22,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get(CONF_HOST)
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
+    devid = config.get(ATTR_ENTITY_ID, None)
 
     #if host is None or username is None or password is None:
     #    _LOGGER.error('Invalid config. Expected %s, %s and %s',
@@ -37,14 +39,16 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     #    return False
 
     # Add devices
-    add_devices(AwesomeLight(light) for light in [0,1])
+    #add_devices(AwesomeLight(light) for light in [0,1])
+    add_devices([AwesomeLight(devid)])
 
 class AwesomeLight(enocean.EnOceanDevice,ToggleEntity):
     """Represents an AwesomeLight in Home Assistant."""
 
-    def __init__(self, light):
+    def __init__(self, devid):
         """Initialize an AwesomeLight."""
         enocean.EnOceanDevice.__init__(self)
+        self._devid = devid
         self._light = None
         self._on_state = False
         self._on_state2 = False
@@ -57,14 +61,14 @@ class AwesomeLight(enocean.EnOceanDevice,ToggleEntity):
         """
         #self._light.update()
 
-    @property
-    def brightness(self):
-        """Brightness of the light.
+    #@property
+    #def brightness(self):
+    #    """Brightness of the light.
 
-        This method is optional. Removing it indicates to Home Assistant
-        that brightness is not supported for this light.
-        """
-        return 50
+    #    This method is optional. Removing it indicates to Home Assistant
+    #    that brightness is not supported for this light.
+    #    """
+    #    return 50
 
     @property
     def is_on(self):
@@ -73,19 +77,30 @@ class AwesomeLight(enocean.EnOceanDevice,ToggleEntity):
 
     def turn_on(self, **kwargs):
         print("TUrning on")
-        a = bytearray(b'\x55\x00\x0A\x00\x01\x80\xA5\x02\x64\x01\x09\xFF\xC6\xEA\x01\x00\x6E/')
+        #a = bytearray(b'\x55\x00\x0A\x00\x01\x80\xA5\x02\x64\x01\x09\xFF\xC6\xEA\x01\x00\x6E/')
         # nattbord
         #self.send_command(data=[0xD2,0x01,0x00,0x64,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x91,0x3d,0xe7,0xff,0x00],packet_type=0x01)
         # ovn_stue
-        self.send_command(data=[0xD2,0x01,0x00,0x64,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x94,0x9e,0xca,0xff,0x00],packet_type=0x01)
+        #self.send_command(data=[0xD2,0x01,0x00,0x64,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x94,0x9e,0xca,0xff,0x00],packet_type=0x01)
+        # tvbenk
+        optional = [0x03,]
+        optional.extend(self._devid)
+        optional.extend([0xff, 0x00])
+        self.send_command(data=[0xD2,0x01,0x00,0x64,0x00,0x00,0x00,0x00,0x00],optional=optional,packet_type=0x01)
         self._on_state = True
 
     def turn_off(self, **kwargs):
         print("TUrning off")
-        a = bytearray(b'\x55\x00\x0A\x00\x01\x80\xA5\x02\x00\x01\x08\xFF\xC6\xEA\x01\x00\xB9/')
+        #a = bytearray(b'\x55\x00\x0A\x00\x01\x80\xA5\x02\x00\x01\x08\xFF\xC6\xEA\x01\x00\xB9/')
         # nattbord
         #self.send_command(data=[0xD2,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x91,0x3d,0xe7,0xff,0x00],packet_type=0x01)
         # ovn_stue
-        self.send_command(data=[0xD2,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x94,0x9e,0xca,0xff,0x00],packet_type=0x01)
+        #self.send_command(data=[0xD2,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x94,0x9e,0xca,0xff,0x00],packet_type=0x01)
+        # tvbenk
+        optional = [0x03,]
+        optional.extend(self._devid)
+        optional.extend([0xff, 0x00])
+        self.send_command(data=[0xD2,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00],optional=optional,packet_type=0x01)
+        #self.send_command(data=[0xD2,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00],optional=[0x03,0x01,0x90,0x84,0x3c,0xff,0x00],packet_type=0x01)
         self._on_state = False
 
