1af0b60e0353135072e67c438ffb1825b62f2889
Ruben Undheim <ruben.undheim@gmail.com>
2016-04-27 08:54:46 +0200

    Changed to binary sensor

--- homeassistant/components/binary_sensor/enocean.py
+++ homeassistant/components/binary_sensor/enocean.py
@@ -1,18 +1,23 @@
 from homeassistant.const import TEMP_CELCIUS
 from homeassistant.helpers.entity import Entity
+from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.components import enocean
 from homeassistant.const import ATTR_ENTITY_ID
 
 DEPENDENCIES = ["enocean"]
 
+cnt = 1
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    global cnt
     devid = config.get(ATTR_ENTITY_ID, None)
-    add_devices([ExampleSensor(devid)])
+    add_devices([ExampleSensor(devid,cnt)])
+    cnt = cnt + 1
 
 
-class ExampleSensor(enocean.EnOceanDevice,Entity):
+class ExampleSensor(enocean.EnOceanDevice,BinarySensorDevice):
 
-    def __init__(self,devid):
+    def __init__(self,devid,cnt):
         enocean.EnOceanDevice.__init__(self)
         self.stype = "listener"
         self.temp_temperature = 0
@@ -20,10 +25,11 @@ class ExampleSensor(enocean.EnOceanDevice,Entity):
         self.sensorid = devid
         self.which = -1
         self.onoff = -1
+        self._cnt = cnt
 
-    #@property
-    #def name(self):
-    #    return 'Temperature'
+    @property
+    def name(self):
+        return 'Trykknapp %d' % self._cnt
 
     def value_changed(self,value,value2):
         self.temp_temperature = value
@@ -46,7 +52,7 @@ class ExampleSensor(enocean.EnOceanDevice,Entity):
 
     #@property
     #def state(self):
-    #    return self.temp_temperature
+    #    return False # self.temp_temperature
 
     #@property
     #def unit_of_measurement(self):
