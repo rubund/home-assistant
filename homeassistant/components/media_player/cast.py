92555b9046410fdf9e71b1ec318aaf08c5192356
Ruben Undheim <ruben.undheim@gmail.com>
2016-05-16 11:10:19 +0200

    Show media player even when chromecast isn't available

--- homeassistant/components/media_player/cast.py
+++ homeassistant/components/media_player/cast.py
@@ -66,6 +66,18 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     add_devices(casts)
 
+def check_if_open(func):
+    def func_wrapper(*args, **kwargs):
+        self = args[0]
+        if self.cast == None:
+            try:
+                self.setup_device(self.host,self.port)
+                return func(*args, **kwargs)
+            except Exception:
+                print("\n\n##########################\n\nFailed opening chromecast: %s\n\n######################\n\n" % str(Exception))
+                self.cast = None
+                return None
+    return func_wrapper
 
 class CastDevice(MediaPlayerDevice):
     """Representation of a Cast device on the network."""
@@ -74,6 +86,15 @@ class CastDevice(MediaPlayerDevice):
     # pylint: disable=too-many-public-methods
     def __init__(self, host, port):
         """Initialize the Cast device."""
+        self.host = host
+        self.port = port
+        try:
+            self.setup_device(host,port)
+        except Exception:
+            print("\n\n##########################\n\nFailed opening chromecast: %s\n\n######################\n\n" % str(Exception))
+            self.cast = None
+
+    def setup_device(self, host, port):
         import pychromecast
         self.cast = pychromecast.Chromecast(host, port)
 
@@ -90,12 +111,14 @@ class CastDevice(MediaPlayerDevice):
         return False
 
     @property
+    @check_if_open
     def name(self):
         """Return the name of the device."""
         return self.cast.device.friendly_name
 
     # MediaPlayerDevice properties and methods
     @property
+    @check_if_open
     def state(self):
         """Return the state of the player."""
         if self.media_status is None:
@@ -112,21 +135,25 @@ class CastDevice(MediaPlayerDevice):
             return STATE_UNKNOWN
 
     @property
+    @check_if_open
     def volume_level(self):
         """Volume level of the media player (0..1)."""
         return self.cast_status.volume_level if self.cast_status else None
 
     @property
+    @check_if_open
     def is_volume_muted(self):
         """Boolean if volume is currently muted."""
         return self.cast_status.volume_muted if self.cast_status else None
 
     @property
+    @check_if_open
     def media_content_id(self):
         """Content ID of current playing media."""
         return self.media_status.content_id if self.media_status else None
 
     @property
+    @check_if_open
     def media_content_type(self):
         """Content type of current playing media."""
         if self.media_status is None:
@@ -140,11 +167,13 @@ class CastDevice(MediaPlayerDevice):
         return None
 
     @property
+    @check_if_open
     def media_duration(self):
         """Duration of current playing media in seconds."""
         return self.media_status.duration if self.media_status else None
 
     @property
+    @check_if_open
     def media_image_url(self):
         """Image url of current playing media."""
         if self.media_status is None:
@@ -155,60 +184,72 @@ class CastDevice(MediaPlayerDevice):
         return images[0].url if images else None
 
     @property
+    @check_if_open
     def media_title(self):
         """Title of current playing media."""
         return self.media_status.title if self.media_status else None
 
     @property
+    @check_if_open
     def media_artist(self):
         """Artist of current playing media (Music track only)."""
         return self.media_status.artist if self.media_status else None
 
     @property
+    @check_if_open
     def media_album(self):
         """Album of current playing media (Music track only)."""
         return self.media_status.album_name if self.media_status else None
 
     @property
+    @check_if_open
     def media_album_artist(self):
         """Album arist of current playing media (Music track only)."""
         return self.media_status.album_artist if self.media_status else None
 
     @property
+    @check_if_open
     def media_track(self):
         """Track number of current playing media (Music track only)."""
         return self.media_status.track if self.media_status else None
 
     @property
+    @check_if_open
     def media_series_title(self):
         """The title of the series of current playing media (TV Show only)."""
         return self.media_status.series_title if self.media_status else None
 
     @property
+    @check_if_open
     def media_season(self):
         """Season of current playing media (TV Show only)."""
         return self.media_status.season if self.media_status else None
 
     @property
+    @check_if_open
     def media_episode(self):
         """Episode of current playing media (TV Show only)."""
         return self.media_status.episode if self.media_status else None
 
     @property
+    @check_if_open
     def app_id(self):
         """Return the ID of the current running app."""
         return self.cast.app_id
 
     @property
+    @check_if_open
     def app_name(self):
         """Name of the current running app."""
         return self.cast.app_display_name
 
     @property
+    @check_if_open
     def supported_media_commands(self):
         """Flag of media commands that are supported."""
         return SUPPORT_CAST
 
+    @check_if_open
     def turn_on(self):
         """Turn on the ChromeCast."""
         # The only way we can turn the Chromecast is on is by launching an app
@@ -221,48 +262,59 @@ class CastDevice(MediaPlayerDevice):
             self.cast.play_media(
                 CAST_SPLASH, pychromecast.STREAM_TYPE_BUFFERED)
 
+    @check_if_open
     def turn_off(self):
         """Turn Chromecast off."""
         self.cast.quit_app()
 
+    @check_if_open
     def mute_volume(self, mute):
         """Mute the volume."""
         self.cast.set_volume_muted(mute)
 
+    @check_if_open
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         self.cast.set_volume(volume)
 
+    @check_if_open
     def media_play(self):
         """Send play commmand."""
         self.cast.media_controller.play()
 
+    @check_if_open
     def media_pause(self):
         """Send pause command."""
         self.cast.media_controller.pause()
 
+    @check_if_open
     def media_previous_track(self):
         """Send previous track command."""
         self.cast.media_controller.rewind()
 
+    @check_if_open
     def media_next_track(self):
         """Send next track command."""
         self.cast.media_controller.skip()
 
+    @check_if_open
     def media_seek(self, position):
         """Seek the media to a specific location."""
         self.cast.media_controller.seek(position)
 
+    @check_if_open
     def play_media(self, media_type, media_id):
         """Play media from a URL."""
         self.cast.media_controller.play_media(media_id, media_type)
 
     # Implementation of chromecast status_listener methods
+    @check_if_open
     def new_cast_status(self, status):
         """Called when a new cast status is received."""
         self.cast_status = status
         self.update_ha_state()
 
+    @check_if_open
     def new_media_status(self, status):
         """Called when a new media status is received."""
         self.media_status = status
